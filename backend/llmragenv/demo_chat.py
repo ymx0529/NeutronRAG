113e8084e9b10d240391981896ab535ee2c1f124
lpz <1565561624@qq.com>
2025-03-21 10:38:09 +0800

    format chat

--- backend/llmragenv/demo_chat.py
+++ backend/llmragenv/demo_chat.py
@@ -2,7 +2,7 @@
 Author: lpz 1565561624@qq.com
 Date: 2025-03-19 20:28:13
 LastEditors: lpz 1565561624@qq.com
-LastEditTime: 2025-03-21 10:28:18
+LastEditTime: 2025-03-21 10:37:48
 FilePath: /lipz/NeutronRAG/NeutronRAG/backend/llmragenv/demo_chat.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -64,6 +64,11 @@ class Demo_chat:
         self.graphdb = GraphDBFactory("nebulagraph").get_graphdb(space_name='rgb')
         self.chat_graph = ChatGraphRAG(self.llm, self.graphdb)
         self.chat_vector = ChatVectorRAG(self.llm,self.vectordb)
+        #TODO 这里我就不定义 chat_hybrid了，避免多次查询。 写的时候可以先根据自己的理解重构ChatGraphRAG，ChatVectorRAG中的检索并传入Demo_chat类中的相关参数，
+        #然后在这个类中再定义一个hybrid_chat()方法，和定义存储查询结果的属性
+
+
+        
 
     def load_llm(self, model_name, url, api_key):
         try:
